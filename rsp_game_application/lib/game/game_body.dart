27c45266e3ae8edabc5c1b2f5dfc106e2d8d58be
junhyeong7788 <ryker77@naver.com>
2023-12-27 13:45:05 +0900

    enum.dart add

--- rsp_game_application/lib/game/game_body.dart
+++ rsp_game_application/lib/game/game_body.dart
@@ -11,14 +11,26 @@ class GameBody extends StatefulWidget {
 }
 
 class _GameBodyState extends State<GameBody> {
+  late bool isDone;
+  //late는 나중에 값을 할당한다는 의미
+  //bool은 true, false를 가지는 자료형
+  //result는 게임의 결과를 저장하는 변수
+
+  @override
+  void initState() {
+    //initState는 위젯이 생성될 때 호출되는 함수
+    super.initState(); //super는 부모 클래스를 의미 //initState는 부모 클래스의 initState를 호출한다.
+    isDone = false; //결과값이 false일때 작동
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Column(
       //Column은 세로로 위젯을 배치하는 위젯
       children: [
-        Expanded(child: CpuInput()),
-        Expanded(child: GameResult()),
-        Expanded(child: UserInput())
+        Expanded(child: CpuInput(isDone: isDone)),
+        Expanded(child: GameResult(isDone: isDone)),
+        Expanded(child: UserInput(isDone: isDone))
       ],
     );
   }
--- rsp_game_application/lib/game/widget/game_result.dart
+++ rsp_game_application/lib/game/widget/game_result.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class GameResult extends StatelessWidget {
-  const GameResult({super.key});
+  final bool isDone; //final : 변수를 상수화 시킨다.
+
+  const GameResult({required this.isDone, super.key});
 
   @override
   Widget build(BuildContext context) {
